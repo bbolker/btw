3aa2f8213bddf8ef6345243413c66c62c770addf
Ben Bolker <bbolker@gmail.com>
2024-08-11 21:17:58 -0400

    better header parsing (BT v4?)

--- R/parse_schedule.R
+++ R/parse_schedule.R
@@ -1,20 +1,20 @@
 # FUNCTION TO PARSE SCHEDULE FILES
 parse_schedule <- function(file) {
 
-# SCAN SCHEDULE FILE
-out <- scan(file = file, what="c", quiet=T, sep="\n")
+    out <- scan(file = file, what="c", quiet=TRUE, sep="\n")
 
-# WHERE DOES HEADER START?
-header_start <- grep("Rate Tried", out)
+    sched_start <- grep("Default schedule", out)
+    if (length(sched_start)==0) sched_start <- 0
+    header_start <- grep("Rate Tried", out)
+    
+    ## SEPARATE SCHEDULE AND HEADER
+    schedule <- read.table(file, skip = sched_start, nrow = (header_start - 1 - sched_start), sep = "\t", header = FALSE)
+    names(schedule) <- c("operator","percent_tried")
+    
+    header <- read.table(file, skip = (header_start - 1), sep = "\t", header = TRUE)
+    header <- header[,-ncol(header)]
 
-# SEPARATE SCHEDULE AND HEADER
-schedule <- read.table(file, nrow = (header_start - 1), sep = "\t", header = FALSE)
-names(schedule) <- c("operator","percent_tried")
-header<- read.table(file, skip = (header_start - 1), sep = "\t", header = TRUE)
-header<- header[,-ncol(header)]
-
-# RETURN LIST
-Schedule <- list(schedule=schedule, header=header)
-return(Schedule)
-
+    Schedule <- list(schedule=schedule, header=header)
+    return(Schedule)
+    
 }
